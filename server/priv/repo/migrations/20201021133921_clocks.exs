e9f2d4110808778f34f873fb5a9d8b9ac6b5cb69
Nymrinae <nymrinaedev@gmail.com>
2020-10-21 16:08:41 +0200

    set install migration

--- server/priv/repo/migrations/20201021133921_clocks.exs
+++ server/priv/repo/migrations/20201021133921_clocks.exs
@@ -2,6 +2,13 @@ defmodule Server.Repo.Migrations.Clocks do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      add :username, :string
+      add :email, :string
+
+      timestamps()
+    end
+
     create table(:clocks) do
       add :time, :utc_datetime
       add :status, :boolean
@@ -9,6 +16,5 @@ defmodule Server.Repo.Migrations.Clocks do
 
       timestamps()
     end
-
   end
 end
