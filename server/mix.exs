bf219c90f6d8726906f4fe1d04b6601ccdbde8db
Nymrinae <nymrinaedev@gmail.com>
2020-10-21 17:25:02 +0200

    set exdoc pkg

--- server/mix.exs
+++ server/mix.exs
@@ -42,7 +42,8 @@ defmodule Server.Mixfile do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_doc, "~> 0.13", only: :dev}
     ]
   end
 
