0ecab2c10c01169492b1f0908a47b1191ff29704
Warren-o <warren.o-connor@epitech.eu>
2020-10-23 14:04:43 +0200

    added empty clock class

---
+++ client/src/api/ClockManager.ts
@@ -0,0 +1,18 @@
+import { $axios } from '@/helpers/axiosInstance'
+
+const clockIn = async () => {
+}
+
+const  refresh = async () => {
+
+}
+
+const clock = async () => {
+
+}
+
+export {
+    clockIn,
+    refresh,
+    clock
+}
