24c75dc8c9d1937c337f507554a30157c004e7df
Nymrinae <nymrinaedev@gmail.com>
2020-10-28 15:45:25 +0100

    setup default layout

---
+++ client/src/pages/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div> test </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component({ layout: 'dashboard' })
+export default class DashboardPage extends Vue {}
+</script>
+
+<style scoped>
+</style>
