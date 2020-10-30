25b02017e8b03a4065fe2a271058ca14d36f9b0b
Nymrinae <nymrinaedev@gmail.com>
2020-10-30 16:38:26 +0100

    add team in user table

--- client/src/pages/admin/users.vue
+++ client/src/pages/admin/users.vue
@@ -12,7 +12,7 @@
     </thead>
     <tbody>
       <tr
-        v-for="{ id, username, email, role } in filteredUsers"
+        v-for="{ id, username, email, team, role } in filteredUsers"
         :key="id"
         class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
       >
@@ -31,6 +31,10 @@
         <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
           <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Role</span>
           {{ role }}
+        </td>
+        <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+          <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Team</span>
+          {{ team }}
         </td>
         <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
           <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Actions</span>
@@ -56,21 +60,21 @@ const AppModule = namespace('app')
 @Component({ layout: 'dashboard' })
 export default class Users extends Vue {
   @AppModule.State searchedUser
-  categories = ['ID', 'Username', 'Email', 'Role', 'Actions']
+  categories = ['ID', 'Username', 'Email', 'Role', 'Team', 'Actions']
 
   get filteredUsers() {
     return this.users.filter(u => u.username.toLowerCase().indexOf(this.searchedUser.toLowerCase()) != -1)
   }
 
   users = [
-    { id: 1, username: 'Nymrinae', email: 'menfou@gmail.com', role: 'Admin', team: 'None' },
-    { id: 2, username: 'Seraphae', email: 'menfou@gmail.com', role: 'User', team: 'None' },
-    { id: 3, username: 'Saphyrae', email: 'menfou@gmail.com', role: 'User', team: 'None' },
-    { id: 4, username: 'Test', email: 'menfou@gmail.com', role: 'User', team: 'None' },
-    { id: 5, username: 'Pseudo', email: 'menfou@gmail.com', role: 'User', team: 'None' },
-    { id: 6, username: 'Irelia', email: 'menfou@gmail.com', role: 'Manager', team: 'None' },
-    { id: 7, username: 'Renekton', email: 'menfou@gmail.com', role: 'Manager', team: 'None' },
-    { id: 8, username: 'Fiora', email: 'menfou@gmail.com', role: 'User', team: 'None' }
+    { id: 1, username: 'Nymrinae', email: 'menfou@gmail.com', role: 'Admin', team: 'TimeManager_1' },
+    { id: 2, username: 'Seraphae', email: 'menfou@gmail.com', role: 'User', team: 'TimeManager_1' },
+    { id: 3, username: 'Saphyrae', email: 'menfou@gmail.com', role: 'User', team: 'TimeManager_1' },
+    { id: 4, username: 'Test', email: 'menfou@gmail.com', role: 'User', team: 'TimeManager_7' },
+    { id: 5, username: 'Pseudo', email: 'menfou@gmail.com', role: 'User', team: 'TimeManager_7' },
+    { id: 6, username: 'Irelia', email: 'menfou@gmail.com', role: 'Manager', team: 'TimeManager_3' },
+    { id: 7, username: 'Renekton', email: 'menfou@gmail.com', role: 'Manager', team: 'TimeManager_3' },
+    { id: 8, username: 'Fiora', email: 'menfou@gmail.com', role: 'User', team: 'TimeManager_3' }
   ]
 }
 </script>
