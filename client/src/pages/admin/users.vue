215282579b7d6c3b7e04fa4b6ded86c215cd15f5
Nymrinae <nymrinaedev@gmail.com>
2020-10-30 11:01:30 +0100

    setup basic team && admin pages and update breadcrumb

---
+++ client/src/pages/admin/users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> Admin can view all users here </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({ layout: 'dashboard' })
+export default class Users extends Vue {
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/pages/team/members.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({ layout: 'dashboard' })
+export default class Members extends Vue {
+
+}
+</script>
+
+<style scoped>
+</style>
