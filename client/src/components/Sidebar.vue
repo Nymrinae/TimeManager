170088b9281dcc952aa768bef12b6dbd440f4cc4
Nymrinae <nymrinaedev@gmail.com>
2020-10-30 10:14:26 +0100

    change font && add working button

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -7,7 +7,7 @@
     id="main-nav"
   >
     <div class="w-full h-20 border-b flex px-4 items-center mb-8">
-      <p class="font-semibold text-3xl text-blue-400 pl-4">Time Manager</p>
+      <p class="font-semibold text-2xl text-blue-400 pl-4">Time Manager</p>
     </div>
 
     <div class="mb-4 px-4">
--- client/src/layouts/dashboard.vue
+++ client/src/layouts/dashboard.vue
@@ -22,7 +22,13 @@
           </nav>
           <div class="lg:flex justify-between items-center mb-6">
             <p class="text-2xl font-semibold mb-2 lg:mb-0">Hello, Nymrinae!</p>
-            <button class="bg-blue-500 hover:bg-blue-600 focus:outline-none rounded-lg px-6 py-2 text-white font-semibold shadow">Action Here</button>
+            <button
+              class="bg-red-500 hover:bg-red-600 focus:outline-none rounded-lg px-6 py-2 text-white font-semibold shadow"
+              :class="{ 'bg-blue-500 hover:bg-blue-600': working }"
+              @click="working = !working"
+            >
+              Working
+            </button>
           </div>
           <router-view />
         </div>
@@ -39,6 +45,8 @@ const AppModule = namespace('app')
 @Component
 export default class Dashboard extends Vue {
   @AppModule.State sidebarOpen
+
+  working: boolean = false
 
 }
 </script>
